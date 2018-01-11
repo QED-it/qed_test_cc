421794fb3d10fbb4c2c082c3427841d4fbe17b94
Kobi Gurkan <kobigurk@gmail.com>
2018-01-11 22:00:00 +0200

    Adds test code for secpk problem

---
+++ cmd/sacc/sacc.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
+)
+
+type SimpleAsset struct {
+}
+
+func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	fmt.Printf("Hey!!")
+	secp256k1.CompressPubkey(big.NewInt(0), big.NewInt(1))
+	return shim.Success(nil)
+}
+
+func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+	return shim.Success([]byte("OK"))
+}
+
+func main() {
+	if err := shim.Start(new(SimpleAsset)); err != nil {
+		fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
+	}
+}
---
+++ vendor/github.com/milagro-crypto/amcl/version3/java/README.md
@@ -0,0 +1,25 @@
+Namespaces are used to separate different curves.
+
+To build the library and see it in action, execute the python3 scripts 
+config32.py or config64.py (depending on whether you want a 32 or 
+64-bit build), and select the curves that you wish to support. The 
+configured library can be built using maven. 
+
+Tests will take a while to  run.
+
+As a quick example copy to a working directory and execute
+
+py config64.py
+
+or perhaps
+
+python3 config64.py
+
+Choose options 1, 17 and 23, for example.
+
+Once the library is configured, you can compile and install with maven:
+
+cd amcl
+mvn clean install
+
+Testing will be carried out during the installation process.
